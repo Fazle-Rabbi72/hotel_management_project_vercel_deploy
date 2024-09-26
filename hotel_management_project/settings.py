b143ce303a091d55b9ba8c591622988e9035022b
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-09-26 15:21:32 -0600

    CSRF_rrusted_origins_added

--- hotel_management_project/settings.py
+++ hotel_management_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-jtx_g%s+d!4420&v531coxrue_6gl_-!#a*==o8lhmwn8c-(@z
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://natures-paradise-stlb.onrender.com/','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://natures-paradise-stlb.onrender.com','https://*.127.0.0.1']
 AUTH_USER_MODEL = "user.User"
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
