90e552449f5268b91a03c668c661bcc96c5faf61
Fazle-Rabbi72 <fazlerabbi0172291@gmail.com>
2024-09-28 06:31:32 -0600

    activation link add

--- user/views.py
+++ user/views.py
@@ -34,7 +34,7 @@ class UserRegistrationView(APIView):
           print("token",token)
           uid=urlsafe_base64_encode(force_bytes(user.pk))
           print("uid",uid)
-          confirm_link=f"http://127.0.0.1:8000/register/active/{uid}/{token}/"
+          confirm_link=f"https://natures-paradise-stlb.onrender.com/register/active/{uid}/{token}/"
           email_subject="Confirm Your Email"
           email_body=render_to_string('confirm_mail.html',{"confirm_link":confirm_link})
           email=EmailMultiAlternatives(email_subject,'',to=[user.email])
